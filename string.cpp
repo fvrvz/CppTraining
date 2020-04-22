42325fbfcdb77536200b92df098b3e103251d376
FaraazKhhan <codefaraaz@gmail.com>
2020-04-22 15:34:22 +0530

    gitignore file added and modified string.cpp file

--- string.cpp
+++ string.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-	std::string str = "Hi! my name is Faraaz";
+	std::string str = "Hi! my name is Faraaz, and you entered the next line...";
+	std::string str2;
 
-	std::cout<<str;
+	std::cout<<"Enter any string and press enter: ";
+	getline(std::cin, str2);
+
+	std::cout<<str<<std::endl;
+	std::cout<<str2<<std::endl;
 	return 0;
 }
